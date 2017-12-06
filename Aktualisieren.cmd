d0146d132931c0b3a42f8d658cd66c3d166737d4
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2017-12-06 14:12:53 +0100

    Alle Projektordner nach git kopieren

--- Aktualisieren.cmd
+++ Aktualisieren.cmd
@@ -1,4 +1,9 @@
 copy "c:\git\Logo8-Starter\Logo8_Starter\bin\Debug\Logo8_Starter.exe" "s:\Linder Kurt\Logo8\*.*"
 copy "c:\git\TIA-Portal-V14-Starter\TIA_Portal_V14_SP1_Starter\bin\Debug\TIA_Portal_V14_SP1_Starter.exe" "s:\Linder Kurt\TIA Portal V14 SP1\*.*"
 copy "c:\git\TwinCAT-V3-Starter\TwinCAT_V3_Starter\bin\Debug\TwinCAT_V3_Starter.exe" "s:\Linder Kurt\TwinCAT_V3\*.*"
+
+xcopy "s:\Linder Kurt\Logo8\Projekte" "c:\git\Projekte_Logo8_V8.0"  /k/h/e/v/c/q/r 
+
+
 pause
+
