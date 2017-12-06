ae855940755a6cf47f11ce4949b1bad2b9e33957
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2017-12-06 13:46:30 +0100

    Batch Datei zum aktualisieren aller exe erstelle

---
+++ Aktualisieren.cmd
@@ -0,0 +1,4 @@
+copy "c:\git\Logo8-Starter\Logo8_Starter\bin\Debug\Logo8_Starter.exe" "s:\Linder Kurt\Logo8\*.*"
+copy "c:\git\TIA-Portal-V14-Starter\TIA_Portal_V14_SP1_Starter\bin\Debug\TIA_Portal_V14_SP1_Starter.exe" "s:\Linder Kurt\TIA Portal V14 SP1\*.*"
+copy "c:\git\TwinCAT-V3-Starter\TwinCAT_V3_Starter\bin\Debug\TwinCAT_V3_Starter.exe" "s:\Linder Kurt\TwinCAT_V3\*.*"
+pause
